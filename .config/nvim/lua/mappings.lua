ed0ab2506c8d314e717074c03b98e49ad9af1df3
yamaguchitokuma <tokuma@yamaguchitokumanonotobukkukonpyuta.local>
2024-06-22 18:10:51 +0900

    .config/nvimを移植

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,12 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("n", "<leader>fb", ":Telescope file_browser path=%:p:h previewer=false select_buffer=true<CR>", { desc = "file browser" })
+map("n", "<leader>fw", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "live grep args" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,88 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier"
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+
+  {
+    "github/copilot.vim",
+    lazy=false,
+  },
+
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   branch = "canary",
+  --   dependencies = {
+  --     { "github/copilot.vim" },
+  --     { "nvim-lua/plenary.nvim" },
+  --   },
+  --   opts = {
+  --     debug = true, -- Enable debugging
+  --     -- See Configuration section for rest
+  --   },
+  --   lazy = false,
+  --   -- See Commands section for default commands if you want to lazy load on them
+  -- },
+
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+
+    require("telescope").setup({
+      theme = "decay",
+      defaults = {
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+          },
+        },
+        border = {},
+      },
+      extensions_list = { "themes", "terms" },
+    })
+  }
+}
