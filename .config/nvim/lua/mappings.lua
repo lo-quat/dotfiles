809d7e6028e40ab35edf100eecfb1c78cd520fdd
Tokuma Yamaguchi <tokuma.yamaguchi@gmail.com>
2024-07-28 17:14:20 +0900

    Set default initial mode for Telescope buffers

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>fb", ":Telescope buffers initial_mode=normal<CR>")
 map("n", "<leader>fw", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "Telescope live grep args" })
 map("n", "<leader>gt", ":Telescope git_status initial_mode=normal<CR>", { desc = "Telescope file browser" })
 map("n", "<leader>gc", ":CopilotChat<CR>", { desc = "GitHub Copilot chat" })
