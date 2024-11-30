93ad8ddd3398e1e9ce467da47ac6853f54b0906e
Tokuma Yamaguchi <tokuma.yamaguchi@gmail.com>
2024-11-30 14:47:09 +0900

    Remove dependencies from CopilotChat

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -47,10 +47,6 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim" },
-    },
     opts = {
       debug = true, -- Enable debugging
       -- See Configuration section for rest
