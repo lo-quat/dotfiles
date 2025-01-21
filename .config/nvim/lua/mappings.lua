c62dfedceae213cc80a519461d1d2f9b52c42dd1
lo-quat <tokuma.yamaguchi@gmail.com>
2025-01-21 23:17:22 +0900

    Add telescope grep word under cursor mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,6 +9,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>fb", ":Telescope buffers initial_mode=normal<CR>")
 map("n", "<leader>fw", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = "Telescope live grep args" })
+map("n", "<leader>fg", ":lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<CR>", { desc = "Telescope grep word under cursor" })
 map("n", "<leader>gt", ":Telescope git_status initial_mode=normal<CR>", { desc = "Telescope Git status" })
 map("n", "<leader>gc", ":CopilotChat<CR>", { desc = "GitHub Copilot chat" })
 map("n", "<leader>yp", ":let @+=expand('%:~:.')<CR>", { desc = "Copy relative path to clipboard" })
@@ -34,4 +35,3 @@ map("n", "<leader>rs", function()
     end,
   }
 end, { desc = "Rspec current line" })
-
